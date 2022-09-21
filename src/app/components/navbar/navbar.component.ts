c9b9470277980657ecbefdebf50e8e9e840fea59
María Isabel Álvarez Hernández <maria.alvarez9409@gmail.com>
2022-09-20 20:12:04 -0500

    show dynamic content in navbar component

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,21 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { filter } from 'rxjs/operators';
+import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
+  user: User;
   activeRoute: string;
   userSubs: Subscription;
   routeSub: Subscription;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private userService: UserService) {}
 
   ngOnInit(): void {
+    this.user = this.userService.user;
+
+    this.userSubs = this.userService.user$.subscribe((user: User) => {
+      this.user = user;
+    });
+
     this.routeSub = this.router.events
       .pipe(filter((event) => event instanceof NavigationEnd))
       .subscribe((event) => {
@@ -23,5 +32,14 @@ export class NavbarComponent implements OnInit {
           this.activeRoute = this.router.url;
         }
       });
+  }
+
+  logout() {
+    this.userService.logout();
+  }
+
+  ngOnDestroy() {
+    this.userSubs?.unsubscribe();
+    this.routeSub?.unsubscribe();
   }
 }
